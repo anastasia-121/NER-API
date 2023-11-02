0c099ba2586bdc00f003e14346385672bf9e4207
Al-Radi Malak <61251184+anastasia-121@users.noreply.github.com>
2023-11-02 14:31:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from app.api.endpoints import recognize_entities, upload_file
 
 app = FastAPI()
+spell = SpellChecker()
+nlp = spacy.load("en_core_web_sm")
+nlp_oliverguhr = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base")
 
 app.include_router(recognize_entities.router)
 app.include_router(upload_file.router)
+app.include_router(spelling_corrector.router)
